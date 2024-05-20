c323b11cb70f6e28655af4a19e306fb876071cc2
Felipe Baldim Guerra <fbaldim.guerra@gmail.com>
2024-05-19 21:19:36 -0300

    Adding exernal api

--- app/api/students/route.ts
+++ app/api/students/route.ts
@@ -4,8 +4,8 @@ import Data from '@/data/students.json'
 // Will be needed when we inset actual classes in a DB
 
 export async function GET() {
-    // const res = await fetch(`${process.env.IP_API}api/view-attendance/?period=Vespertino&schoolGrade=6&classMeta=Português&teacher=1&date=2024-03-31`);
-    // const students = await res.json();
-    const students = Data
+    const res = await fetch(`${process.env.IP_API}api/view-attendance/?period=Vespertino&schoolGrade=6&classMeta=Português&teacher=1&date=2024-03-31`);
+    const students = await res.json();
+    // const students = Data
     return NextResponse.json(students)
 }
